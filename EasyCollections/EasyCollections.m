8fef3f5b3986a896aecc15c8be953c8970aaf072
Shoaib Qureshi <shoaibahmedqureshi@Shoaibs-MacBook-Pro.local>
2017-09-23 03:47:55 +0500

    Initial Commit

---
+++ EasyCollections/EasyCollections.m
@@ -0,0 +1,13 @@
+//
+//  EasyCollections.m
+//  EasyCollections
+//
+//  Created by Shoaib Ahmed Qureshi on 9/23/17.
+//  Copyright © 2017 Shoaib Ahmed Qureshi. All rights reserved.
+//
+
+#import "EasyCollections.h"
+
+@implementation EasyCollections
+
+@end
